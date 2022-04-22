83cecf303796082e97d8bffc23a633a3c6c29df1
Eltsov Dmitriy <technic.dima@yandex.ru>
2022-04-22 20:53:13 +0300

    форма регистрации

--- client/src/components/registrationComponent.js
+++ client/src/components/registrationComponent.js
@@ -5,7 +5,25 @@ class RegistrationComponent extends React.Component {
     render() {
         return (
         <div className='registrationComponent'>
-            
+            <div className="registrationComponent__registrationWrapper">
+                <h1 className="registrationComponent__title">Регистрация</h1>
+                <form>
+                    <p className="registrationComponent__label">Имя пользователя</p>
+                    <input type="text" className="registrationComponent__input" placeholder="Имя пользователя"></input>
+                    <p className="registrationComponent__label">Адрес электронной почты</p>
+                    <input type="text" className="registrationComponent__input" placeholder="Адрес электронной почты"></input>
+                    <div className="registrationComponent__labelContainer">
+                        <p className="registrationComponent__label">Пароль</p>
+                        <p className="registrationComponent__forgotPassword">Забыли пароль?</p>
+                    </div>
+                    <input type="text" className="registrationComponent__input" placeholder="Пароль"></input>
+                    <div className="registrationComponent__checkboxWrapper">
+                        <input type="checkbox" className="registrationComponent__checkbox"></input>
+                        <p className="registrationComponent__label registrationComponent__label_checkbox">Запомнить меня</p>
+                    </div>
+                    <button className="registrationComponent__button">Зарегистрироваться</button>
+                </form>
+            </div>
         </div>);
     }
 }
