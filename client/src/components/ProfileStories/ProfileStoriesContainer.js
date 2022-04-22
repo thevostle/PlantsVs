6a6e8e51b1d57e36885d40b952352deaa2b5e2e9
Ivan <vanca624@gmail.com>
2022-04-23 02:22:31 +0300

    Some other layouts

--- client/src/components/ProfileStories/ProfileStoriesContainer.js
+++ client/src/components/ProfileStories/ProfileStoriesContainer.js
@@ -4,7 +4,7 @@ import './ProfileStoriesContainer.scss';
 
 const ProfileStoriesContainer = () => (
     <div className="profileStoriesContainer">
-        <div className="profileStoriesContainer__title"></div>
+        <div className="profileStoriesContainer__title">Новости</div>
         <div className="profileStoriesContainer__itemsContainer">
             // тут будут элементы-истории
             <ProfileStory />
--- client/src/views/ProfilePage/ProfilePage.js
+++ client/src/views/ProfilePage/ProfilePage.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 import ProfileBio from '../../components/ProfileBio/ProfileBio';
 import ProfileNews from '../../components/ProfileNews/ProfileNews';
+import ProfileStoriesContainer from '../../components/ProfileStories/ProfileStoriesContainer';
 
 import './ProfilePage.scss';
 
@@ -11,8 +12,27 @@ const ProfilePage = () => {
     return (
         <div className="profilePage">
             {username}
-            <ProfileBio />
-            <ProfileNews />
+            <div className="profilePage__collumn profilePage__collumn_left">
+                <ProfileBio />
+            </div>
+            <div className="profilePage__collumn">
+                <ProfileStoriesContainer />
+                <ProfileNews />
+                <ProfileNews />
+                <ProfileNews />
+                <ProfileNews />
+                <ProfileNews />
+                <ProfileNews />
+                <ProfileNews />
+                <ProfileNews />
+                <ProfileNews />
+                <ProfileNews />
+                <ProfileNews />
+                <ProfileNews />
+            </div>
+            <div className="profilePage__collumn">
+                
+            </div>
         </div>
     );
 };
