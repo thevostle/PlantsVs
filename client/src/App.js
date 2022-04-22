601eea518196a7abaf323a27e83d120dc724af5c
Eltsov Dmitriy <technic.dima@yandex.ru>
2022-04-22 20:27:01 +0300

    начало работы над регистрацией

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import logo from './logo.svg';
-import './App.css';
+import RegistrationPage from "./components/pages/registrationPage";
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Switch>
+            <Route exact path='/'>
+                <RegistrationPage />
+            </Route>
+            <Route path="/:username">
+              
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
---
+++ client/src/components/registrationComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/components/registrationComponent.scss';
+
+class RegistrationComponent extends React.Component {
+    render() {
+        return (
+        <div className='registrationComponent'>
+            
+        </div>);
+    }
+}
+
+export default RegistrationComponent;
